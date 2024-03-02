9d5b7530adb354409e86c7c8e9f7c391542ade81
Daniel Edwards <danieledwards@macbook-pro.myfiosgateway.com>
2024-03-02 14:30:21 -0500

    module 7 critical thinking

---
+++ CSC500_Module_7_Critical_Thinking_Edwards_Daniel.py
@@ -0,0 +1,42 @@
+class_information = {
+        
+    "room_number": {
+        "CSC101": 3004,
+        "CSC102": 4501,
+        "CSC103": 6755,
+        "NET110": 1244,
+        "COM241": 1411
+    },
+    
+    "class_instructors": {
+        "CSC101": "Haynes",
+        "CSC102": "Alvarado",
+        "CSC103": "Rich",
+        "NET110": "Burke",
+        "COM241": "Lee"
+    },
+    
+    "meeting_times": {
+        "CSC101": "8:00 a.m.",
+        "CSC102": "9:00 a.m.",
+        "CSC103": "10:00 a.m.",
+        "NET110": "11:00 a.m.",
+        "COM241": "1:00 p.m."
+    }
+}
+
+while True:
+    class_number = input("Enter a class number or 'q' to quit: ")
+
+    if class_number in class_information["room_number"] and class_number in class_information["class_instructors"] and class_number in class_information["meeting_times"]:
+        room_number = class_information["room_number"][class_number]
+        instructor = class_information["class_instructors"][class_number]
+        meeting_time = class_information["meeting_times"][class_number]
+        print(f"\n{class_number} is taught by Professor {instructor}, and meets at {meeting_time} in classroom number {room_number}.\n")
+    elif class_number == "":
+        print("No class number entered. Please try again.")
+    elif class_number.lower() == 'q':
+        print("\nYou have quit the program")
+        break
+    else:
+        print(f"Information for class number {class_number} is not available.")
